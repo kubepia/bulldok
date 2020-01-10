ab8b7736f3139485b7c70ca07e28d1ab50d3f389
myguddy <seungil.han@gmail.com>
2020-01-10 11:27:27 +0900

    add logger filter for UUID

--- src/main/java/io/nogada/sambulldok/SamBulldokApplication.java
+++ src/main/java/io/nogada/sambulldok/SamBulldokApplication.java
@@ -3,6 +3,8 @@ package io.nogada.sambulldok;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,16 @@ public class SamBulldokApplication {
 	@GetMapping(value = "/health")
 	public String health() {
 	   return "ok";
+	}
+
+	// logger filter setting
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Bean
+	public FilterRegistrationBean loggingFilter() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+
+		registrationBean.setFilter(new MdcLoggingFilter());
+
+		return registrationBean;
 	}
 }
